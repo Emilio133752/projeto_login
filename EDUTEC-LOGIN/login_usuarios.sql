b8ac93c987aa7c6782d85dde78e97dd602286e60
Emilio Santos <75913301+Emilio133752@users.noreply.github.com>
2021-10-23 20:35:22 -0300

    Update login_usuarios.sql

--- EDUTEC-LOGIN/login_usuarios.sql
+++ EDUTEC-LOGIN/login_usuarios.sql
@@ -33,29 +33,11 @@ CREATE TABLE IF NOT EXISTS `login_usuarios` (
   `email` varchar(200) NOT NULL,
   `senha` varchar(200) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=latin1;
 
 --
 -- Extraindo dados da tabela `login_usuarios`
 --
-
-INSERT INTO `login_usuarios` (`id`, `nome`, `email`, `senha`) VALUES
-(3, 'Emilio Santos MendonÃ§a', 'emiliocantil726@gmail.com2', '1c6637a8f2e1f75e06ff9984894d6bd16a3a36a9'),
-(4, 'Emilio Santos MendonÃ§a', 'emiliocantil726@gmail.c', '210427ffde6b0fbad37d3dc5fbcd08d22695da29'),
-(5, 'Xeraca', 'aa@gmail.com', '40bd001563085fc35165329ea1ff5c5ecbdbbeef'),
-(6, 'Emilio Santos MendonÃ§a', 'emliocantil726@gmail.com', '07cea7ad6037661357dad52ee7b257a864db1f30'),
-(7, 'Emilio Santos MendonÃ§a', 'emiliocantil726@gmail.com', '123'),
-(8, 'Emilio Santos MendonÃ§a', 'emilintil726@gmail.com', '12345'),
-(9, 'Emilio Santos MendonÃ§a', 'iocantil726@gmail.com', '123'),
-(10, 'Emilio Santos MendonÃ§a', 'cantil726@gmail.com', '123'),
-(11, 'Emilio Santos MendonÃ§a', 'emiliocantil726@gmail.', '123'),
-(12, 'Emilio Santos MendonÃ§a', '26@gmail.com', '33333'),
-(13, 'Emilio Santos MendonÃ§a', 'ocantil726@gmail.com', '123'),
-(14, 'Emilio Santos MendonÃ§a', 'emiliocantil726gmail.com', '123313123'),
-(15, 'Emilio Santos MendonÃ§a', 'ntil726@gmail.com', '1111111111111111'),
-(16, 'Emilio Santos MendonÃ§a', 'aauaua@gmail.com', '1234567890'),
-(17, 'Emilio Santos MendonÃ§a', '@gmail.com', '1231231231');
-COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
